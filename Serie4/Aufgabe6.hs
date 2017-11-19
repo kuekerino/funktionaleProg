20f414eaa7a21537bfec6365b539b4485c081908
j3ff3r <j3ff3r@protonmail.com>
2017-11-19 15:35:32 +0100

    Update Aufgabe6.hs

--- Serie4/Aufgabe6.hs
+++ Serie4/Aufgabe6.hs
@@ -8,3 +8,13 @@ diffList [] _ = []
 diffList (x:xs) (y:ys)
     | x == y = diffList xs ys
     | otherwise = diffList [x] ys ++ diffList xs (y:ys)
+    
+    
+{-
+diffList :: Ord a => [a] -> [a] -> [a]
+diffList x [] = x
+diffList [] _ = []
+diffList (x:xs) (y:ys) 
+ | not (x `elem` (y:ys))  = x : diffList xs (y:ys)
+ | otherwise   = diffList xs (delete x (y:ys))
+-}
