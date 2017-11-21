b203de8763c5bc1470ee39b4c778c3283fca1b52
kuekerino <kuekerino@kuekerino.local>
2017-11-21 13:05:21 +0100

    verbeserung nach Forderung

--- Serie5/Aufgabe3.hs
+++ Serie5/Aufgabe3.hs
@@ -1,6 +1,6 @@
 potList :: Int -> Int -> [Int]
-potList x y = unique $ [1] ++ [a^b | a <- [2..x],
-                     b <- [1..y]]
+potList x y = unique $ [a^b | a <- [2..x],
+                     b <- [2..y]]
 
 unique :: [Int] -> [Int]
 unique xs = [x | (x,y) <- zip xs [0..], x `notElem` (take y xs)]
